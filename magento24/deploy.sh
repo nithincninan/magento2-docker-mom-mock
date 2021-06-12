6a407274e8488195f045ab91e82c6e7cb841c42f
Nithincninan <nithin@corra.com>
2021-06-13 01:13:56 +0530

    m2-2.4.2-docker-mom-mock

--- magento24/deploy.sh
+++ magento24/deploy.sh
@@ -3,4 +3,5 @@
 php bin/magento setup:upgrade
 php bin/magento setup:di:compile
 php -dmemory_limit=6G bin/magento setup:static-content:deploy -f
+chown -R www-data:www-data .
 #chmod -R 777 var pub app/etc/* generated
